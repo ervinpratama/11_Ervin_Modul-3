f82ea06e4946533571c42468cee501f05169a9b7
vinsifa <vinpratama12@gmail.com>
2022-03-07 10:45:08 +0700

    Modul 3-React

---
+++ src/Component/HelloComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+    return <p className='test-p'> ini adalah arrow function yang ada di folder </p>
+}
+
+// agar component ini dapat dipakai dimana aja
+export default HelloComponent;
